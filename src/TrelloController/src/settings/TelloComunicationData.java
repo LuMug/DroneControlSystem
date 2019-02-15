f5c55525c63207964e0b6fec3f71e23600810b1a
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-02-15 14:40:17 +0100

    Update controller

---
+++ src/TrelloController/src/settings/TelloComunicationData.java
@@ -0,0 +1,13 @@
+package settings;
+
+
+/**
+ *
+ * @author Luca Di Bello
+ */
+public abstract class TelloComunicationData {
+    public static final String TELLO_ADDRESS = "192.168.10.1";
+    public static final int TELLO_COMMAND_SEND_PORT = 8889;
+    public static final int TELLO_STATE_LISTEN_PORT = 8890;
+    public static final int TELLO_VIDEO_LISTEN_PORT = 11111;
+}
