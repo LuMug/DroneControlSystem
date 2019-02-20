2faa3e82ff49f1b3b18d7824dd539b3309d7fadd
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-02-20 15:45:48 +0100

    Update Comunication features

--- src/DroneController/src/reader/LeapMotionListener.java
+++ src/DroneController/src/reader/LeapMotionListener.java
@@ -7,11 +7,13 @@ import com.leapmotion.leap.*;
  * @author Fadil Smajilbasic
  */
 public class LeapMotionListener extends Listener {
-
+    
+    @Override
     public void onConnect(Controller controller) {
         System.out.println("Connected");
     }
 
+    @Override
     public void onFrame(Controller controller) {
         Frame frame = controller.frame();
         
@@ -21,6 +23,5 @@ public class LeapMotionListener extends Listener {
                    + ", fingers: " + frame.fingers().count()
                    + ", left Hand: " + frame.hands().get(0).palmPosition()
                    + ", rigth Hand: " + frame.hands().get(1).palmPosition());
-        
     }
 }
--- src/DroneController/src/settings/TelloComunicationData.java
+++ src/DroneController/src/settings/TelloComunicationData.java
@@ -10,6 +10,8 @@ public abstract class TelloComunicationData {
     public static final String JARI_ADDRESS = "192.168.43.47";
     
     public static final int TELLO_COMMAND_SEND_PORT = 8889;
+    public static final int TELLO_COMMAND_LISTEN_PORT = 8889;
+    
     public static final int TELLO_STATE_LISTEN_PORT = 8890;
     public static final int TELLO_VIDEO_LISTEN_PORT = 11111;
 }
