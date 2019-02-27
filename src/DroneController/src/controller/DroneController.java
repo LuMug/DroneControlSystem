d5ea2856abe08eaf850364d9b71bc7ce4e6c293a
duck <fadil.smajilbasic@samtrevano.ch>
2019-02-27 14:36:08 +0100

    update

--- src/DroneController/src/controller/DroneController.java
+++ src/DroneController/src/controller/DroneController.java
@@ -7,45 +7,63 @@ import com.leapmotion.leap.Listener;
 import comunication.Commands;
 import comunication.CommandManager;
 import reader.LeapMotionReader;
+import reader.LeapMotionReaderListener;
 
 /**
  *
  * @author Luca Di Bello
  */
-public class DroneController extends Commands{
+public class DroneController extends Listener {
+
     private static CommandManager commandManager = new CommandManager();
-    private static LeapMotionReader leapReader = new LeapMotionReader();
+    private LeapMotionReader leapReader;
+    private Frame frame;
+    private Controller controller;
 
     private static final float STEP = 10f;
-    
-    
-    public static void sendUpCommand(float distance){
-        commandManager.sendCommand(up(distance));
-    }
-    
+
+//    public static void sendUpCommand(float distance) {
+//        commandManager.sendCommand(up(distance));
+//    }
+
     public static void main(String[] args) {
         System.out.println("Started Controller :)");
-        
+        Controller controller = new Controller();
         //Reads leapmotion data and sends it to the drone
         while(true){
-            Frame frame = leapReader.getFrame();
-            System.out.println(frame);
-            
-            /*
-            if(righthand != null){
-                float up = translateInput(leapReader.getHandY(righthand), STEP);
-                
-                //sendUpCommand(translateInput(leapReader.getHandY(righthand), STEP));
-                System.out.println("Up: " + up);
-            }   
-            */
+            Frame frame = controller.frame();
+            System.out.println("frame: " + frame);
         }
     }
     
-    public static float translateInput(float altitude, float step){
+    
+    public void onConnect(Controller controller) {
+        System.out.println("Connected");
+    }
+
+    public void onFrame(Controller controller) {
+        
+        setFrame(controller.frame());
+        System.out.println("frame received: " + getFrame());
+    }
+    public synchronized void setFrame(Frame frame) {
+        this.frame = frame;
+    }
+
+    public synchronized Frame getFrame() {
+        return frame;
+    }
+
+    public static float translateInput(float altitude, float step) {
         //MAX 60 CM
         //Punto 0 -> 30 CM
         float translated = (altitude - 30) % step;
         return translated;
     }
+
+
+    public DroneController() {
+//        leapReader = new LeapMotionReader();
+    }
+    
 }
