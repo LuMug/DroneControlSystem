8ee5566e606b76d6e7f56195cc1ada83a8ef59ec
duck <fadil.smajilbasic@samtrevano.ch>
2019-02-27 16:20:05 +0100

    DroneController working and other updates

--- src/DroneController/src/controller/DroneController.java
+++ src/DroneController/src/controller/DroneController.java
@@ -16,53 +16,30 @@ import reader.LeapMotionReaderListener;
  *
  * @author Luca Di Bello
  */
-public class DroneController extends Listener {
+public class DroneController extends Listener implements Runnable {
 
     private static CommandManager commandManager = new CommandManager();
     private static LeapMotionReader leapReader;
-    private static Frame frame;
     private Controller controller;
 
     private static final float STEP = 10f;
 
     private static final int AVERAGE_POINTS = 10;
 
-    public DroneController(){
+    public DroneController() {
         controller = new Controller();
         controller.addListener(this);
-
-        try {
-            System.in.read();
-        } catch (IOException ex) {
-            System.err.println("IOException io.read:" + ex.getMessage());
-        }
-    }
+        leapReader = new LeapMotionReader();
     }
 
-    public synchronized void setFrame(Frame frame) {
-        this.frame = frame;
-    }
 
-    public synchronized Frame getFrame() {
-        return frame;
+    public static void sendUpCommand(float distance) {
+        commandManager.sendCommand(Commands.up(distance));
     }
-
-//    public static void sendUpCommand(float distance) {
-//        commandManager.sendCommand(up(distance));
-//    }
     public static void main(String[] args) {
         System.out.println("Started Controller :)");
         DroneController controller = new DroneController();
-        //Reads leapmotion data and sends it to the drone
-        while (true) {
-            float altitudeCm = controller.translateAltitude(controller.getAverageAltitude(AVERAGE_POINTS), STEP);
-
-            if (altitudeCm > 0) {
-                System.out.println("Up: " + altitudeCm);
-            } else {
-                System.out.println("Down: " + altitudeCm);
-            }
-        }
+        controller.run();
     }
 
     public void onConnect(Controller controller) {
@@ -70,10 +47,9 @@ public class DroneController extends Listener {
     }
 
     public void onFrame(Controller controller) {
-
-        setFrame(controller.frame());
-        System.out.println("frame received: " + getFrame());
-
+        leapReader.setFrame(controller.frame());
+        System.out.println(leapReader.getHandY());
+        sendUpCommand(translateAltitude(leapReader.getHandY(),STEP));
     }
 
     public float translateAltitude(float altitude, float step) {
@@ -88,10 +64,10 @@ public class DroneController extends Listener {
         int total = 0;
 
         for (int i = 0; i < points; i++) {
-            Hand rHand = leapReader.getRightHand(getFrame());
+            Hand rHand = leapReader.getRightHand();
 
             if (rHand != null) {
-                total += leapReader.getHandY(rHand);
+                total += leapReader.getHandY();
             } else {
                 //Reset
                 i--;
@@ -101,25 +77,15 @@ public class DroneController extends Listener {
         return total / points;
     }
 
-
-    public void onConnect(Controller controller) {
-        System.out.println("Connected");
+    @Override
+    public void run() {
+        System.out.println("reading");
+        try {
+            System.in.read();
+        } catch (IOException ex) {
+            System.err.println("IOException io.read:" + ex.getMessage());
+        }
+        System.out.println("finished reading");
     }
 
-    public void onFrame(Controller controller) {
-        setFrame(controller.frame());
-        //System.out.println("Controller: " +controller.frame().currentFramesPerSecond());
-        //System.out.println("Frame: " + getFrame().currentFramesPerSecond());
-
-        System.out.println(getFrame().hands().rightmost().palmPosition().getY());
-        //System.out.println(leapReader.getHandY(getFrame().hands().rightmost()));
-    }
-
-    public synchronized void setFrame(Frame frame) {
-        this.frame = frame;
-    }
-
-    public static synchronized Frame getFrame() {
-        return frame;
-    }
 }
