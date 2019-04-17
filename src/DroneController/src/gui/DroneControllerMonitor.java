1f1cfa17fa0aaf1d57784434596f7069858dda17
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-04-17 14:40:37 +0200

    update

--- src/DroneController/src/gui/DroneControllerMonitor.java
+++ src/DroneController/src/gui/DroneControllerMonitor.java
@@ -1,8 +1,6 @@
 package gui;
 
 import controller.DroneController;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import settings.SettingsManager;
 import javax.swing.JOptionPane;
@@ -21,10 +19,8 @@ public class DroneControllerMonitor extends javax.swing.JFrame implements Comman
      * Creates new form DroneControllerMonitor
      */
     public DroneControllerMonitor() {
-
         initComponents();
         updateSettingsValues(manager);
-
     }
 
     public void setListener(SettingsListener listener) {
@@ -47,6 +43,17 @@ public class DroneControllerMonitor extends javax.swing.JFrame implements Comman
         jPanePageLog = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jPanelFastCommands = new javax.swing.JPanel();
+        jPanelMoveInSpace = new javax.swing.JPanel();
+        jButtonDroneUp = new javax.swing.JButton();
+        jButtonDroneDown = new javax.swing.JButton();
+        jButtonDroneLeft = new javax.swing.JButton();
+        jButtonDroneRight = new javax.swing.JButton();
+        jPanelAbortPanel = new javax.swing.JPanel();
+        jButtonAbortFlight = new javax.swing.JButton();
+        jPanelExtraCommands = new javax.swing.JPanel();
+        jButtonDroneFlip = new javax.swing.JButton();
+        jComboBoxFlip = new javax.swing.JComboBox<>();
         jPanelSettings = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         sensibilityValueTextBox = new javax.swing.JTextField();
@@ -88,6 +95,55 @@ public class DroneControllerMonitor extends javax.swing.JFrame implements Comman
         jPanePageLog.add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
         jTabbedPane1.addTab("Log", jPanePageLog);
+
+        jPanelFastCommands.setLayout(new java.awt.GridLayout(2, 0));
+
+        jPanelMoveInSpace.setBorder(javax.swing.BorderFactory.createTitledBorder("Move in space"));
+        jPanelMoveInSpace.setLayout(new java.awt.BorderLayout());
+
+        jButtonDroneUp.setText("UP");
+        jPanelMoveInSpace.add(jButtonDroneUp, java.awt.BorderLayout.PAGE_START);
+
+        jButtonDroneDown.setText("DOWN");
+        jPanelMoveInSpace.add(jButtonDroneDown, java.awt.BorderLayout.PAGE_END);
+
+        jButtonDroneLeft.setText("LEFT");
+        jPanelMoveInSpace.add(jButtonDroneLeft, java.awt.BorderLayout.LINE_START);
+
+        jButtonDroneRight.setText("RIGHT");
+        jPanelMoveInSpace.add(jButtonDroneRight, java.awt.BorderLayout.LINE_END);
+
+        jPanelAbortPanel.setLayout(new java.awt.GridBagLayout());
+
+        jButtonAbortFlight.setBackground(new java.awt.Color(255, 0, 0));
+        jButtonAbortFlight.setText("ABORT MISSION!");
+        jButtonAbortFlight.setCursor(new java.awt.Cursor(java.awt.Cursor.CROSSHAIR_CURSOR));
+        jButtonAbortFlight.setMargin(new java.awt.Insets(10, 10, 10, 10));
+        jButtonAbortFlight.setPreferredSize(new java.awt.Dimension(120, 60));
+        jButtonAbortFlight.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonAbortFlightActionPerformed(evt);
+            }
+        });
+        jPanelAbortPanel.add(jButtonAbortFlight, new java.awt.GridBagConstraints());
+
+        jPanelMoveInSpace.add(jPanelAbortPanel, java.awt.BorderLayout.CENTER);
+
+        jPanelFastCommands.add(jPanelMoveInSpace);
+
+        jPanelExtraCommands.setBorder(javax.swing.BorderFactory.createTitledBorder("Extra commands :)"));
+        jPanelExtraCommands.setLayout(new java.awt.BorderLayout());
+
+        jButtonDroneFlip.setText("FLIP IT :0");
+        jPanelExtraCommands.add(jButtonDroneFlip, java.awt.BorderLayout.CENTER);
+
+        jComboBoxFlip.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Forward", "Right", "Down", "Left" }));
+        jComboBoxFlip.setSelectedIndex(0);
+        jPanelExtraCommands.add(jComboBoxFlip, java.awt.BorderLayout.PAGE_START);
+
+        jPanelFastCommands.add(jPanelExtraCommands);
+
+        jTabbedPane1.addTab("Fast Commands", jPanelFastCommands);
 
         jPanelSettings.setLayout(new java.awt.GridLayout(6, 2));
 
@@ -171,9 +227,12 @@ public class DroneControllerMonitor extends javax.swing.JFrame implements Comman
         this.updateSettingsValues(manager);
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void jButtonAbortFlightActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAbortFlightActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonAbortFlightActionPerformed
+
     public void notifyChangeSettings() {
         listener.settingsChanged();
-
     }
 
     /**
@@ -209,7 +268,8 @@ public class DroneControllerMonitor extends javax.swing.JFrame implements Comman
             public void run() {
                 dcm.setVisible(true);
             }
-        });
+        });   
+        
         DroneController controller = new DroneController();
         controller.setListener(dcm);
         dcm.setListener(controller);
@@ -234,6 +294,13 @@ public class DroneControllerMonitor extends javax.swing.JFrame implements Comman
     private javax.swing.JTextField heightPointsValueTextBox;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButtonAbortFlight;
+    private javax.swing.JButton jButtonDroneDown;
+    private javax.swing.JButton jButtonDroneFlip;
+    private javax.swing.JButton jButtonDroneLeft;
+    private javax.swing.JButton jButtonDroneRight;
+    private javax.swing.JButton jButtonDroneUp;
+    private javax.swing.JComboBox<String> jComboBoxFlip;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -241,8 +308,12 @@ public class DroneControllerMonitor extends javax.swing.JFrame implements Comman
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanePageLog;
+    private javax.swing.JPanel jPanelAbortPanel;
     private javax.swing.JPanel jPanelBody;
+    private javax.swing.JPanel jPanelExtraCommands;
+    private javax.swing.JPanel jPanelFastCommands;
     private javax.swing.JPanel jPanelHeader;
+    private javax.swing.JPanel jPanelMoveInSpace;
     private javax.swing.JPanel jPanelSettings;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
