5b73509736b99bdd52ecc71daf43fcd7ca3ca927
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-05-20 08:48:36 +0200

    Fix javadoc

--- src/DroneController/src/settings/FlipCommand.java
+++ src/DroneController/src/settings/FlipCommand.java
@@ -6,11 +6,22 @@ package settings;
 public enum FlipCommand {
     LEFT("l"), RIGHT("r"), FORWARD("f"), BACK("b");
     
+    /**
+     * Value of the enum.
+     */
     private String value; 
     
+    /**
+     * Parametrized constructor.
+     * @param value Value which will be the enum value.
+     */
     private FlipCommand(String value){
         this.value = value;
     }
     
+    /**
+     * Getter method for getting the enum string value.
+     * @return Value of the enum as String
+     */
     public String getValue() { return value; }
 }
